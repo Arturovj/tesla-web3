1499adeb4cc1925c63e64575caf3b696e2d46b85
Arturo Vivar <arthas1237@hotmail.com>
2022-05-19 23:22:50 +0200

    added spinner

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,16 +1,37 @@
 import React from 'react'
-import { FooterContainer } from '../../containers/footer'
+import GridLoader from "react-spinners/GridLoader"
+import './Home.css'
 
 import Hover from '../Hover/Hover'
 import WorldCanvas from '../World/WorldCanvas'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+
+    const [loading, setLoading] = useState(false)
+    useEffect(() =>{
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        },1500)
+    }, [])
+
+
+
   return (
     <>
+    <div className='Home'>
+
+   <div className='loading-home'>
+    { loading ? <GridLoader  color={'#D0021B'}  loading={loading} size={100}/> : 
      <WorldCanvas/>
-     <Hover/>
-    
-    
+    }
+    </div>
+   
+
+    <Hover/>
+     
+    </div>
     </>
   )
 }
