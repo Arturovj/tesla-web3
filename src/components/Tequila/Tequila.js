e051dd5e2dd92b01f53598a10f1f784f8e96439e
Arturo Vivar <arthas1237@hotmail.com>
2022-05-19 12:23:01 +0200

    tequila added

--- src/components/Tequila/Tequila.js
+++ src/components/Tequila/Tequila.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import { Suspense } from "react";
+import "./Tequila.css";
+import styled from "styled-components";
 
-export default function Tequila() {
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+
+
+// import World from "../GLTF/World";
+import Tequila from "../GLTF/Tequila";
+
+
+export default function TequilaCanvas() {
   return (
-    <div>Tequila</div>
-  )
+    <Wrapper className="world">
+      <Canvas clasName="canvas">
+        <OrbitControls enableZoom={true} autoRotate autoRotateSpeed={1.5}  />
+        <ambientLight intensity={5} />
+        <spotLight intensity={10} angle={20} penumbra={0.01} position={[-10,20,-5]} castShadow></spotLight>
+        <directionalLight position={[-1, 5, 1]} />
+        <Suspense fallback={null}>
+          <Tequila />
+        </Suspense>
+        </Canvas>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+  margin: 0 auto;
+
+  canvas {
+    height: 500px;
+    width: auto;
+    
+  }
+`;
