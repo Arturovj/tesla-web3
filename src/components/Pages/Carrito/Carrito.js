214f6541c8a07b2e7884113519adc15d55db63f7
Arturo Vivar <arthas1237@hotmail.com>
2022-05-26 17:46:34 +0200

    terminamos por hoy

--- src/components/Pages/Carrito/Carrito.js
+++ src/components/Pages/Carrito/Carrito.js
@@ -1,62 +1,68 @@
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useCarritoContext } from '../../../contexts/carritoContext'
-import { useUserContext } from '../../../contexts/userContext';
-import createCheckoutSession from '../../../functions/createCheckoutSession';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useCarritoContext } from "../../../contexts/carritoContext";
+import { useUserContext } from "../../../contexts/userContext";
+import createCheckoutSession from "../../../functions/createCheckoutSession";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./Carrito.css";
 
 export default function Carrito() {
-    const { carrito } = useCarritoContext();
-    const { user } = useUserContext()
-    let navigate = useNavigate();
+  const { carrito } = useCarritoContext();
+  const { user } = useUserContext();
+  let navigate = useNavigate();
 
-    console.log(carrito)
+  console.log(carrito);
 
-    function isAuthenticated(){
-        if(user){
-            createCheckoutSession(user.uid, carrito)
-            toast.success('Redirecting!', {
-                position: "top-center",
-                autoClose: 7000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                });
-        }
-        if (!user){
-                console.log("you have to login")
-                navigate("/login");
-        }
+  function isAuthenticated() {
+    if (user && carrito.length >= 1) {
+      createCheckoutSession(user.uid, carrito);
+      toast.success("Redirecting!", {
+        position: "top-center",
+        autoClose: 7000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
 
-  return (
+    if (user && carrito.length < 1){
+        navigate("/shop")
+    }
 
+    if (!user) {
+      console.log("you have to login");
+      navigate("/login");
+    }
+  }
+
+  return (
     <div>
-        <ToastContainer
-position="top-center"
-autoClose={7000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-/>
+      <ToastContainer
+        position="top-center"
+        autoClose={7000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />{" "}
+      <div className="cart">
         Tu Carrito:
         {carrito.map((producto) => (
-            <div>
-                <img src={producto.images[0]} alt={producto.name} />
-        <p>{producto.name}</p>
-        </div>
-        
-    ))}
-
-            <button onClick={isAuthenticated}>BUY</button>
-
-        </div>
-  )
+          <div>
+            <img src={producto.images[0]} alt={producto.name} />
+            <p>{producto.name}</p>
+          </div>
+        ))}
+        <button onClick={isAuthenticated}>
+          BUY
+        </button>
+      </div>
+    </div>
+  );
 }
