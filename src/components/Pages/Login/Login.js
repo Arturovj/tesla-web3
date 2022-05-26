460d5a99fd4d0757e58d9bb85cb890fad2947672
Arturo Vivar <arthas1237@hotmail.com>
2022-05-26 17:04:28 +0200

    added toast

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -5,6 +5,9 @@ import "./Login.css";
 import createCheckoutSession from "../../../functions/createCheckoutSession";
 import { useCarritoContext } from "../../../contexts/carritoContext";
 import { useUserContext } from "../../../contexts/userContext";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 
 export default function Login() {
@@ -19,12 +22,40 @@ export default function Login() {
     const email = e.target.email.value;
     const password = e.target.password.value;
     loginEmail(email, password);
-    navigate("/perfil");
+
+
+
+    toast.success('Redirecting!', {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+
+        setTimeout(() => {
+            navigate("/perfil");
+          }, 2000);
+        
+    
     // createCheckoutSession(user.uid, carrito)
   }
 
   return (
     <>
+    <ToastContainer
+position="top-center"
+autoClose={2000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+/>
     <div className="body">
      <div className="main">
   <input type="checkbox" id="chk" aria-hidden="true" />
