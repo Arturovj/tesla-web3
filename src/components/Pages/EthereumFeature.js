cf5c2621d87601d5fa2ba6fd6fc260613da1e721
Arturo Vivar <arthas1237@hotmail.com>
2022-05-31 17:54:10 +0200

    fixed2

--- src/components/Pages/EthereumFeature.js
+++ src/components/Pages/EthereumFeature.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function EthereumFeature() {
   return (
       <>
-    <FontAwesomeIcon icon={faGithub }/>
+   
     <p>ETHEREUM</p>
     </>
   )
