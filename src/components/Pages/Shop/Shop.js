2017165a865f0f5d2a679396bdeef2f524b25f83
Arturo Vivar <arthas1237@hotmail.com>
2022-06-01 22:39:50 +0200

    li motion

--- src/components/Pages/Shop/Shop.js
+++ src/components/Pages/Shop/Shop.js
@@ -14,7 +14,20 @@ const imageAnimate = {
   },
 };
 
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+};
 
+const item = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
 
 export default function Shop() {
   const [productos, setProductos] = useState(null);
@@ -36,38 +49,44 @@ export default function Shop() {
   }, []);
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
+    // <motion.div
+    //   initial={{ opacity: 0 }}
+    //   animate={{ opacity: 1 }}
+    //   exit={{ opacity: 0 }}
+    // >
+    <>
       <div className="shop">
         {loading ? (
           <div className="loader">
             <HashLoader color="red" size={100} />
           </div>
         ) : (
-          <motion.div
-            initial={"offscreen"}
-            whileInView={"onscreen"}
-            viewport={{ once: false, amount: 0.5 }}
-            transition={{ staggerChildren: 0.5 }}
-            variants={imageAnimate}
-          >
-            <div>
-              <ul>
-                {productos
-                  ? productos.map((p) => (
-                      <li style={{ color: "white" }} key={p.id}>
-                        <ItemCard product={p} />
-                      </li>
-                    ))
-                  : null}
-              </ul>
-            </div>
-          </motion.div>
+          // <motion.div
+          //   initial={"offscreen"}
+          //   whileInView={"onscreen"}
+          //   viewport={{ once: false, amount: 0.5 }}
+          //   transition={{ staggerChildren: 0.5 }}
+          //   variants={imageAnimate}
+          // >
+          <div>
+            <motion.ul variants={container} initial="hidden" animate="show">
+              {productos
+                ? productos.map((p) => (
+                    <motion.li
+                      variants={item}
+                      style={{ color: "white" }}
+                      key={p.id}
+                    >
+                      <ItemCard product={p} />
+                    </motion.li>
+                  ))
+                : null}
+            </motion.ul>
+          </div>
+          // </motion.div>
         )}
       </div>
-    </motion.div>
+      {/* </motion.div> */}
+    </>
   );
 }
